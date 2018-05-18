759f3a7afca8875506bc96415e0153c0eeb72090
Sebastian Leal <sebastian.leal@globant.com>
2018-05-18 16:44:01 -0500

    itis added de property configurtion for mongoDB

--- src/main/java/com/project/todo/tasks/configuration/MongoConfiguration.java
+++ src/main/java/com/project/todo/tasks/configuration/MongoConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
-@EnableMongoRepositories(basePackages = "com.project.todo.tasks.repository")
+@EnableMongoRepositories
 public class MongoConfiguration {
 
 }
