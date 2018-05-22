dd1b380c07ce2d2a9f84f8f582233a1fd039de5f
Sebastian Leal <sebastian.leal@globant.com>
2018-05-22 16:56:24 -0500

    added the status code response

--- src/main/java/com/project/todo/tasks/rest/ITaskRest.java
+++ src/main/java/com/project/todo/tasks/rest/ITaskRest.java
@@ -1,6 +1,7 @@
 package com.project.todo.tasks.rest;
 
 import com.project.todo.tasks.document.Task;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,26 +10,34 @@ import java.util.List;
 public interface ITaskRest {
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     void createTask(@RequestBody Task task);
 
     @PatchMapping(path = "/{id}/status")
+    @ResponseStatus(HttpStatus.OK)
     void switchStatus(@PathVariable String id);
 
     @PatchMapping(path = "/{id}/todo")
+    @ResponseStatus(HttpStatus.OK)
     void modifyToDO(@RequestBody Task task, @PathVariable String id);
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     List<Task> getAllTasks();
 
     @GetMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
     Task findByIdTask(@PathVariable String id);
 
     @GetMapping(path = "nickname/{user}")
+    @ResponseStatus(HttpStatus.OK)
     Task findByUserNickname(@PathVariable String user);
 
     @DeleteMapping(path ="/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     void deleteTaskById(@PathVariable String id);
 
     @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     void updateTask(@RequestBody Task task,@PathVariable String id);
 }
