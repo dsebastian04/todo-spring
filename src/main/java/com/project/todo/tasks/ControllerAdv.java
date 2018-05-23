f9e1d52d3ff2f3e66580a3817e40b14abaad2032
Sebastian Leal <sebastian.leal@globant.com>
2018-05-23 16:45:17 -0500

    added controllerAdvice for manage error messages

---
+++ src/main/java/com/project/todo/tasks/ControllerAdv.java
@@ -0,0 +1,36 @@
+package com.project.todo.tasks;
+
+import com.project.todo.tasks.Exception.ApiError;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ControllerAdv extends ResponseEntityExceptionHandler {
+
+    public ControllerAdv() {
+        super();
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return handleExceptionInternal(ex,getApiError(ex,status), headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return handleExceptionInternal(ex,getApiError(ex,status), headers, status, request);
+    }
+
+    private ApiError getApiError(final Exception ex, final HttpStatus httpStatus){
+    final String message = ex.getMessage() == null ? ex.getClass().getSimpleName() :ex.getMessage();
+    final String devMessage = ex.getClass().getSimpleName();
+
+    return new ApiError(httpStatus.value(),message,devMessage);
+    }
+}
